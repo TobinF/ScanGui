4d3b5735e803d1dc3b4b50e538d6249ff663b6c4
TobinF <debinfeng@outlook.com>
2023-09-01 15:04:19 +0800

    display interface

---
+++ app/config/displayItems.py
@@ -0,0 +1,10 @@
+
+displayDict = {
+            '温度': 'temperatureDisplay',
+            '电流': 'currDisplay',
+            '电压': 'voltDisplay',
+            '流量': 'flowDisplay',
+            '压力': 'pressureDisplay',
+            '温度变送器': 'transmitterDisplay',
+            '其他': 'otherDisplay',
+        }
